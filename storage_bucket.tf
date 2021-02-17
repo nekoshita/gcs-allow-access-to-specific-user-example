fe70160f9babc1758ede6217129ab0bcd03c8a95
Nekoshita Yuki <yuki.303442@gmail.com>
2021-02-18 02:04:28 +0900

    fix: add enable uniform option

--- storage_bucket.tf
+++ storage_bucket.tf
@@ -2,6 +2,9 @@ resource "google_storage_bucket" "private_bucket_project_owner_and_one_user_can_
   name     = "private_bucket_project_owner_and_one_user_can_access"
   location = var.gcp_regions["tokyo"]
 
+  # アクセス制御をUniformモードにする
+  uniform_bucket_level_access = true
+
   # このオプションをtrueにしておくと、バケット内にデータが残っていても削除してくれる
   # 本番運用時はtrueにしない方がおそらくいいが、これはサンプルのためtrueにしておく
   force_destroy = true
